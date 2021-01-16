9eeb1ba7c399767e15a9a4ea6c49266c48d9deb2
bernard frimpong <nardfrimpong@gmail.com>
2021-01-16 12:07:30 +0000

    fixed errors

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
-
+const articleRouter = require('./routes/articles')
 app.set('view engine','ejs')
 
+app.use('/articles',articleRouter)
+
 app.get('/',(req,res) => {
-    res.render('index')
+    const articles = [{
+        title: 'test article',
+        createdAt: new Date(),
+        description: "test description"
+    },
+    {
+        title: 'test article',
+        createdAt: new Date(),
+        description: "test description"
+    }
+    ]
+    res.render('articles/index',{articles})
 })
 
 
