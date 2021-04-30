f77c75e2317987b78b89de253f67a6fee437b8e5
bernard frimpong <nardfrimpong@gmail.com>
2021-04-30 13:58:55 +0000

    fixed bootstrap cdn problems

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const Article = require('./../Models/article')
 
 router.get('/new',(req,res) => {
-    res.render('articles/new',{article: new Article()})
+  res.render('articles/new',{article: new Article()})
+})
+
+router.get('/edit:id',async (req,res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('articles/edit',{article: article})
 })
 
 router.get("/:slug",async(req,res) => {
@@ -27,8 +32,35 @@ router.post('/',async(req,res) => {
     
 })
 
+router.post('/', async(req,res,next) => {
+  req.article = await Article()
+  next()
+},saveArticle('new'))
+
+
+router.put("/:id",async (req,res,next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+},saveArticle('edit'))
+
+
+
 router.delete("/:id", async(req,res) => {
   await Article.findByIdAndDelete(req.params.id)
   res.redirect('/')
 })
+function saveArticle (path){
+  return async(req,res) => {
+    let article = req.article
+      article.title = req.body.title  
+      article.description= req.body.description
+      article.markdown = req.body.markdown
+    try {
+     article = await article.save()
+     res.redirect(`/articles/${article.slug}`)
+    } catch (error) {
+      res.render(`articles/${path}`,{article})  
+    }
+  }
+}
 module.exports = router
